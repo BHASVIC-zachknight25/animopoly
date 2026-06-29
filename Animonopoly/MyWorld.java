799f3027e07b6f90cbccf73c8907dcb74cad39ac
BHASVIC-JackLeachman25 <jack.leachman25@students.bhasvic.ac.uk>
2026-06-29 14:33:41 +0100

    Add files via upload

---
+++ Animonopoly/MyWorld.java
@@ -0,0 +1,94 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 700, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+
+        Space Space1 = new Space(1);
+        addObject(Space1,50,650);
+        Space Space2 = new Space(2);
+        addObject(Space2,50,550);
+        Space Space3 = new Space(3);
+        addObject(Space3,50,450);
+        Space Space4 = new Space(4);
+        addObject(Space4,50,350);
+        Space Space5 = new Space(5);
+        addObject(Space5,50,250);
+        Space Space6 = new Space(6);
+        addObject(Space6,50,150);
+        Space Space7 = new Space(7);
+        addObject(Space7,50,50);
+        Space Space8 = new Space(8);
+        addObject(Space8,150,50);
+        Space Space9 = new Space(9);
+        addObject(Space9,250,50);
+        Space Space10 = new Space(10);
+        addObject(Space10,350,50);
+        Space Space11 = new Space(11);
+        addObject(Space11,450,50);
+        Space Space12 = new Space(12);
+        addObject(Space12,550,50);
+        Space Space13 = new Space(13);
+        addObject(Space13,650,50);
+        Space Space14 = new Space(14);
+        addObject(Space14,750,50);
+        Space Space15 = new Space(15);
+        addObject(Space15,750,150);
+        Space Space16 = new Space(16);
+        addObject(Space16,750,250);
+        Space Space17 = new Space(17);
+        addObject(Space17,750,350);
+        Space Space18 = new Space(18);
+        addObject(Space18,750,450);
+        Space Space19 = new Space(19);
+        addObject(Space19,750,550);
+        Space Space20 = new Space(20);
+        addObject(Space20,750,650);
+        Space Space21 = new Space(21);
+        addObject(Space21,650,650);
+        Space Space22 = new Space(22);
+        addObject(Space22,550,650);
+        Space Space23 = new Space(23);
+        addObject(Space23,450,650);
+        Space Space24 = new Space(24);
+        addObject(Space24,350,650);
+        Space Space25 = new Space(25);
+        addObject(Space25,250,650);
+        Space Space26 = new Space(26);
+        addObject(Space26,250,650);
+        Space Space27 = new Space(27);
+        addObject(Space27,150,650);
+       
+       
+       
+       
+       
+       
+       
+       
+    }
+    
+}
